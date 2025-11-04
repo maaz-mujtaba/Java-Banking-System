ddd1fe1ea5148bdfcf5bd1d829869769bd05e84d
Maaz Mujtaba <maaz32418@gmail.com>
2025-11-04 16:10:36 +0530

    "Initial Commit-3"

---
+++ src/Transaction.java
@@ -0,0 +1,41 @@
+import java.sql.Timestamp;
+public class Transaction {
+    int txnId;
+    int accountId;
+    String accType;
+    double amount;
+    String description;
+    Timestamp timestamp;
+    public Transaction(int tId,int accId,String acT,double amt,String msg,Timestamp time){
+        this.txnId = tId;
+        this.accountId = accId;
+        this.accType = acT;
+        this.amount = amt;
+        this.description = msg;
+        this.timestamp = time;
+    }
+    public int getTxnId(){
+        return txnId;
+    }
+    public int getAccountId(){
+        return accountId;
+    }
+    public String getAccType(){
+        return accType;
+    }
+    public double getAmount(){
+        return amount;
+    }
+    public String getDesciption(){
+        return description;
+    }
+    public Timestamp getTime(){
+        return timestamp;
+    }
+    public String toString(){
+        return "Transaction id : " + txnId+
+                "\nAccount Id : " + accountId + "\n"+
+                "Account Type : " + accType + "\n"+
+                "Amount : Rs. " + amount;
+    }
+}
